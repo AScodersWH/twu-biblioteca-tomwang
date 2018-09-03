1bf9156d3b235a95a8d5a0ac2f2013d3a88bb6b7
AScodersWH <895052253@qq.com>
2018-09-03 17:30:46 +0800

    finished all

--- src/com/twu/biblioteca/service/BookStore.java
+++ src/com/twu/biblioteca/service/BookStore.java
@@ -10,26 +10,26 @@ public class BookStore {
 
 
     public List<String> initialBooks() {
-        books.add("Harry Potter");
+        books.add("Harry-Potter");
         books.add("Sea");
         books.add("Sun");
-        books.add("Three guns");
-        books.add("Sam Ports");
+        books.add("Three-guns");
+        books.add("Sam-Ports");
         return books;
     }
 
     public List<String> initialBooksWithDetails() {
-        booksWithDetails.add("Harry Potter     author: JK      time: 2012.12.28      version:  1");
-        booksWithDetails.add("Sea              author: David   time: 2013.11.12      version:  1");
-        booksWithDetails.add("Sun              author: Sam     time: 2009.02.09      version:  1");
-        booksWithDetails.add("Three guns       author: Jan     time: 2007:01.22      version:  1");
-        booksWithDetails.add("Sam Ports        author: Aus     time: 2015.12.01      version:  1");
+        booksWithDetails.add("Harry-Potter-----author:-JK------time:-2012.12.28------version:--1");
+        booksWithDetails.add("Sea--------------author:-David---time:-2013.11.12------version:--1");
+        booksWithDetails.add("Sun--------------author:-Sam-----time:-2009.02.09------version:--1");
+        booksWithDetails.add("Three-guns-------author:-Jan-----time:-2007:01.22------version:--1");
+        booksWithDetails.add("Sam-Ports--------author:-Aus-----time:-2015.12.01------version:--1");
         return booksWithDetails;
     }
 
     public String deleteBook(String bookToLend) {
         for (int i = 0; i < books.size(); i++) {
-            if (books.get(i) == bookToLend) {
+            if (books.get(i).equals(bookToLend)) {
                 books.remove(books.get(i));
                 booksWithDetails.remove(booksWithDetails.get(i));
                 return "Thank you! Enjoy the book.";
@@ -47,10 +47,10 @@ public class BookStore {
 
     public String returnBook(String bookReturn, String booksWithDetialsToreturn) {
         for (String s : books) {
-            if (s == bookReturn) return "That is not a valid book to return.";
+            if (s.equals(bookReturn)) return "That is not a valid book to return.";
         }
-        for (String s : booksWithDetails) {
-            if (s == booksWithDetialsToreturn) return "That is not a valid book to return.";
+        for (String sp : booksWithDetails) {
+            if (sp.equals(booksWithDetialsToreturn)) return "That is not a valid book to return.";
         }
         books.add(bookReturn);
         booksWithDetails.add(booksWithDetialsToreturn);
