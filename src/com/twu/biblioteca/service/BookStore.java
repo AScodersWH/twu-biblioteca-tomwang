eeb42b7d36cced6c9912edd8cc9d3222285911e4
AScodersWH <895052253@qq.com>
2018-09-03 16:26:45 +0800

    some test need add

--- src/com/twu/biblioteca/service/BookStore.java
+++ src/com/twu/biblioteca/service/BookStore.java
@@ -10,29 +10,32 @@ public class BookStore {
 
 
     public List<String> initialBooks() {
-        books.add("1: Harry Potter");
-        books.add("2: Sea");
-        books.add("3: Sun");
-        books.add("4: Three guns");
-        books.add("5: Sam Ports");
+        books.add("Harry Potter");
+        books.add("Sea");
+        books.add("Sun");
+        books.add("Three guns");
+        books.add("Sam Ports");
         return books;
     }
 
     public List<String> initialBooksWithDetails() {
-        booksWithDetails.add("1: Harry Potter     author: JK      time: 2012.12.28      version:  1");
-        booksWithDetails.add("2: Sea              author: David   time: 2013.11.12      version:  1");
-        booksWithDetails.add("3: Sun              author: Sam     time: 2009.02.09      version:  1");
-        booksWithDetails.add("4: Three guns       author: Jan     time: 2007:01.22      version:  1");
-        booksWithDetails.add("5: Sam Ports        author: Aus     time: 2015.12.01      version:  1");
+        booksWithDetails.add("Harry Potter     author: JK      time: 2012.12.28      version:  1");
+        booksWithDetails.add("Sea              author: David   time: 2013.11.12      version:  1");
+        booksWithDetails.add("Sun              author: Sam     time: 2009.02.09      version:  1");
+        booksWithDetails.add("Three guns       author: Jan     time: 2007:01.22      version:  1");
+        booksWithDetails.add("Sam Ports        author: Aus     time: 2015.12.01      version:  1");
         return booksWithDetails;
     }
 
-    public String deleteBook(Integer book) {
-        if (books.size() > book - 1) {
-            books.remove(books.get(book - 1));
-            booksWithDetails.remove(booksWithDetails.get(book - 1));
-            return "Thank you! Enjoy the book.";
-        } else return "That book is not available.";
+    public String deleteBook(String bookToLend) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i) == bookToLend) {
+                books.remove(books.get(i));
+                booksWithDetails.remove(booksWithDetails.get(i));
+                return "Thank you! Enjoy the book.";
+            }
+        }
+        return "That book is not available.";
     }
 
     public void showBooks(List<String> allBooks) {
@@ -40,6 +43,18 @@ public class BookStore {
         for (String s : allBooks) {
             System.out.println(s);
         }
+    }
+
+    public String returnBook(String bookReturn, String booksWithDetialsToreturn) {
+        for (String s : books) {
+            if (s == bookReturn) return "That is not a valid book to return.";
+        }
+        for (String s : booksWithDetails) {
+            if (s == booksWithDetialsToreturn) return "That is not a valid book to return.";
+        }
+        books.add(bookReturn);
+        booksWithDetails.add(booksWithDetialsToreturn);
+        return "Thank you for returning the book.";
     }
 
 
--- src/com/twu/biblioteca/service/WelcomePrintln.java
+++ src/com/twu/biblioteca/service/WelcomePrintln.java
@@ -8,6 +8,7 @@ public class WelcomePrintln {
         System.out.println("1:look for books");
         System.out.println("2:look for books with details");
         System.out.println("3:choose a book you want");
+        System.out.println("4:input your return infos");
         System.out.println("0:exit system");
     }
 }
